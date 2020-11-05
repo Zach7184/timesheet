bc4f96292519f1866cfac43cec827f5ae2cc5c32
Merit Student <student@meac.local>
2020-11-05 13:05:31 -0700

    Save Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
+// Config variables
+const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
+const SHEET_ID = process.env.REACT_APP_SHEET_ID;
+const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
+const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
+console.log(process.env)
+
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+
+const appendSpreadsheet = async (row) => {
+  try {
+    await doc.useServiceAccountAuth({
+      client_email: CLIENT_EMAIL,
+      private_key: PRIVATE_KEY,
+    });
+    // loads document properties and worksheets
+    await doc.loadInfo();
+
+    const sheet = doc.sheetsById[SHEET_ID];
+    const result = await sheet.addRow(row);
+  } catch (e) {
+    console.error('Error: ', e);
+  }
+};
+
+const newRow = { Name: "Zach", Value: "Customer Service" };
+
+appendSpreadsheet(newRow);
+
 function App() {
+  const options = {
+    timeZone:"US/Mountain",
+    hour12 : true,
+    hour:  "2-digit",
+    minute: "2-digit",
+   second: "2-digit"
+ };
+  
+  const inClick = event => {
+    
+    appendSpreadsheet({ Name: new Date().toLocaleTimeString("en-US",options), Value: "in"});
+  }
+  const outClick = event => {
+    appendSpreadsheet({ Name: new Date().toLocaleTimeString("en-US",options), Value: "out" });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={inClick}>In</button>
+      <button onClick={outClick}>Out</button>
     </div>
   );
+
 }
 
 export default App;
